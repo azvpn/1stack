abc7cc47bf1e2b3bf34d445c48ac61be54f0b748
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 11:26:38 +0700

    Add files via upload

---
+++ v2ray/vpndata/1stack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/Nghi235/config/main/vpndata/v2ray/config.yml && cd ../.. && clear
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/setup.sh)
+nano /etc/XrayR/config.yml
